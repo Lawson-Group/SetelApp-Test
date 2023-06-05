074b91532ffd6e50337d4250847380925b422ba7
Aisha Nuryan <91200696+aishaptrn@users.noreply.github.com>
2023-06-05 16:39:08 +0700

    GUI History, GUI Panduan

---
+++ SetelApp/Views/PageHistory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SetelApp
+{
+    public partial class PageHistory : Form
+    {
+        public PageHistory()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PagePanduan guide = new PagePanduan();
+            guide.Show();
+            Visible = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ SetelApp/Views/PagePanduan.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SetelApp
+{
+    public partial class PagePanduan : Form
+    {
+        public PagePanduan()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            PageHistory history = new PageHistory();
+            history.Show();
+            Visible = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
